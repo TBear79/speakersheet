8d0132c728f59e4451fe5eb6bff5cbbeceb9479c
Thorbjørn Jelgren <thorbjorn@therightfoot.dk>
2025-07-09 23:05:24 +0200

    Add page break

--- tools/create-pdf-template/create-pdf-template.js
+++ tools/create-pdf-template/create-pdf-template.js
@@ -34,6 +34,8 @@ const metaJson = {
       memorialTalk: true,
       talks: [
         { talkNumber: 101, language: "Dansk" },
+        { talkNumber: 45, language: "Dansk" },
+        { talkNumber: 213, language: "Dansk" },
         { talkNumber: 205, language: "Engelsk" }
       ]
     },
@@ -46,79 +48,96 @@ const metaJson = {
       talks: [
         { talkNumber: 303, language: "Dansk" }
       ]
-    },
+    }
   ]
 };
 
 const drawSection = (page, font, fontBold, fontSize, x, y, title, lines) => {
   const spacing = 20;
+  let localY = y;
   page.drawText(title, {
     x,
-    y,
+    y: localY,
     size: fontSize + 1,
     font: fontBold,
     color: rgb(0.2, 0.2, 0.2),
   });
-  y -= spacing + 10;
+  localY -= spacing + 10;
 
   let totalLinesHeight = 0;
   lines.forEach(([label, value, delimiter, overrideFont]) => {
     delimiter = delimiter == undefined ? ': ' : delimiter;
     overrideFont = overrideFont == undefined ? font : overrideFont;
     page.drawText(`${label}${delimiter}${value}`, {
-      x: x+10,
-      y,
+      x: x + 10,
+      y: localY,
       size: fontSize,
       font: overrideFont,
       color: rgb(0, 0, 0),
     });
-    y -= spacing;
+    localY -= spacing;
     totalLinesHeight += spacing;
   });
 
+  const boxHeight = (totalLinesHeight + 20);
   page.drawRectangle({
     x: x,
-    y: y + totalLinesHeight + 20,
+    y: y- 10,
     width: 500,
-    height: (totalLinesHeight + 20) * -1,
-    borderColor: rgb(0, 0, 0),
+    height: -boxHeight,
+    borderColor: rgb(0.5, 0.5, 0.5),
     borderWidth: 1.5,
-  })
+  });
 
-  return y - spacing;
+  return boxHeight + 30;
 };
 
 const createPdf = async () => {
   const pdfDoc = await PDFDocument.create();
-  const page = pdfDoc.addPage([595.28, 841.89]); // A4
   const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
   const fontBold = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
   let x = 50;
   let y = 790;
   const fontSize = 11;
+  let pageCount = 1;
 
-  // Header
-  page.drawText(`${metaJson.congregation.officialName} foredragsholdere`, {
-    x,
-    y,
-    size: 16,
-    font: fontBold,
-    color: rgb(0, 0, 0)
-  });
-  page.drawText('Page 1', {
-    x: 520,
-    y,
-    size: 10,
-    font,
-    color: rgb(0.4, 0.4, 0.4)
-  });
-  y -= 30;
+  let currentPage = pdfDoc.addPage([595.28, 841.89]);
+  const drawHeader = () => {
+    currentPage.drawText(`${metaJson.congregation.officialName} foredragsholdere`, {
+      x,
+      y,
+      size: 16,
+      font: fontBold,
+      color: rgb(0, 0, 0)
+    });
+    currentPage.drawText(`Side ${pageCount}`, {
+      x: 520,
+      y,
+      size: 10,
+      font,
+      color: rgb(0.4, 0.4, 0.4)
+    });
+    y -= 30;
+  };
+  drawHeader();
 
-  // Sections
+  const createNewPage = () => {
+    currentPage = pdfDoc.addPage([595.28, 841.89]);
+    y = 790;
+    pageCount++;
+    drawHeader();
+  };
 
+  const minY = 100;
 
+  const drawAutoSection = (title, lines) => {
+    const estimatedHeight = (lines.length + 1) * 20 + 40;
+    if (y - estimatedHeight < minY) createNewPage();
+    const used = drawSection(currentPage, font, fontBold, fontSize, x, y, title, lines);
+    y -= used;
+  };
 
-  y = drawSection(page, font, fontBold, fontSize, x, y, 'Menighed', [
+  drawAutoSection('Menighed', [
     ['Navn', metaJson.congregation.officialName],
     ['Nummer', metaJson.congregation.number],
     ['Adresse', metaJson.congregation.address],
@@ -126,60 +145,57 @@ const createPdf = async () => {
     ['Mødetider', metaJson.congregation.meetingTime]
   ]);
 
-
-  y = drawSection(page, font, fontBold, fontSize, x, y, 'Koordinator for ældsterådet', [
+  drawAutoSection('Koordinator for ældsterådet', [
     ['Navn', metaJson.elderCoordinator.name],
     ['Email', metaJson.elderCoordinator.email],
     ['Telefon', metaJson.elderCoordinator.phone],
     ['Adresse', metaJson.elderCoordinator.address]
   ]);
 
-  y = drawSection(page, font, fontBold, fontSize, x, y, 'Foredragskoordinator', [
-    ['Name', metaJson.talkCoordinator.name],
+  drawAutoSection('Foredragskoordinator', [
+    ['Navn', metaJson.talkCoordinator.name],
     ['Email', metaJson.talkCoordinator.email],
     ['Telefon', metaJson.talkCoordinator.phone]
   ]);
 
-  y -= 16;    
-  
-  page.drawText('Foredragsholdere', {
+  y -= 16;
+  currentPage.drawText('Foredragsholdere', {
     x,
     y,
-    size: fontSize+1,
+    size: fontSize + 1,
     font: fontBold,
     color: rgb(0, 0, 0)
   });
-  y -= 16 + 16;
+  y -= 32;
 
   for (const speaker of metaJson.speakers) {
-    const talkLanguages = speaker.talks.filter(t => t.language)
-    const talks = speaker.talks.map(t => t.talkNumber).join(', ');
-    
     const speakerLines = [
-        ['Telefon', speaker.phone],
-        ['Begravelse', speaker.funeralTalk ? 'Ja' : 'Nej'],
-        ['Bryllup', speaker.weddingTalk ? 'Ja' : 'Nej'],
-        ['Mindehøjtid', speaker.memorialTalk ? 'Ja' : 'Nej'],
-        ['Foredrag', '', '', fontBold]
-      ];
-
-    const talkLines = speaker.talks
-                        .sort(t => t.language)
-                        .map(t => [t.language, speaker.talks.filter(x => x.language === t.language).sort(x => x.talkNumber).map(x => x.talkNumber).join(', ')])
-
-    y = drawSection(page, font, fontBold, fontSize, x, y, `${speaker.name}`, [...speakerLines, ...talkLines]);
-    if (y < 100) {
-      y = 790;
-    }
+      ['Telefon', speaker.phone],
+      ['Begravelse', speaker.funeralTalk ? 'Ja' : 'Nej'],
+      ['Bryllup', speaker.weddingTalk ? 'Ja' : 'Nej'],
+      ['Mindehøjtid', speaker.memorialTalk ? 'Ja' : 'Nej'],
+      ['Foredrag', '', '', fontBold]
+    ];
+    const distinctLanguages = [...new Set(speaker.talks.map(t => t.language))];
+    const talkLines = distinctLanguages
+      .sort((a, b) => a.localeCompare(b))
+      .map(l => [
+        l,
+        speaker.talks
+          .filter(t => t.language === l)
+          .sort((a, b) => a.talkNumber - b.talkNumber)
+          .map(t => t.talkNumber)
+          .join(', ')
+      ]);
+    drawAutoSection(`${speaker.name}`, [...speakerLines, ...talkLines]);
   }
 
-  // Embed _meta field
-  const form = pdfDoc.getForm();
-  const metaField = form.createTextField('_meta');
-  metaField.setText(JSON.stringify(metaJson));
-  metaField.enableReadOnly();
-//   metaField.setHidden(true);
-  form.updateFieldAppearances();
+  currentPage.drawText(JSON.stringify(metaJson), {
+    x: -5000,
+    y: -5000,
+    size: 0.1,
+    color: rgb(1, 1, 1)
+  });
 
   const pdfBytes = await pdfDoc.save();
   await fs.mkdir('./output', { recursive: true });
