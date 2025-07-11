d15f6e13e3315947d5b39f494308f5fd31f51587
Thorbjørn Jelgren <thorbjorn@therightfoot.dk>
2025-07-11 22:36:39 +0200

    Move create pdf logic to common file

---
+++ tools/create-pdf-template/templateData.js
@@ -0,0 +1,63 @@
+export default {
+    "templateVersion":"2025.1.0",
+    "createdBy":"SpeakerSheet",
+    "congregation":{
+       "officialName":"Århus Vestbyen",
+       "nickname":"Vestbyen",
+       "number":"12456",
+       "address":"Vestergade 45, 8000 Århus",
+       "circuit":"S8",
+       "meetingTime":"Søndag 10:30"
+    },
+    "elderCoordinator":{
+       "name":"John Smith",
+       "email":"john@example.com",
+       "phone":"+45 12345678",
+       "address":"Elder Street 5, 1234 City"
+    },
+    "talkCoordinator":{
+       "name":"Peter Jensen",
+       "email":"peter@example.com",
+       "phone":"+45 87654321"
+    },
+    "speakers":[
+       {
+          "name":"Hans Hansen",
+          "phone":"+45 11223344",
+          "funeralTalk":true,
+          "weddingTalk":false,
+          "memorialTalk":true,
+          "talks":[
+             {
+                "talkNumber":101,
+                "language":"Dansk"
+             },
+             {
+                "talkNumber":45,
+                "language":"Dansk"
+             },
+             {
+                "talkNumber":213,
+                "language":"Dansk"
+             },
+             {
+                "talkNumber":205,
+                "language":"Engelsk"
+             }
+          ]
+       },
+       {
+          "name":"Kristian Kristensen",
+          "phone":"+45 99887766",
+          "funeralTalk":false,
+          "weddingTalk":true,
+          "memorialTalk":false,
+          "talks":[
+             {
+                "talkNumber":303,
+                "language":"Dansk"
+             }
+          ]
+       }
+    ]
+ }
