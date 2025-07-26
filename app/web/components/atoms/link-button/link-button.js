d8e6b0b39eeb3f7a000ea1358402d370b218cf91
Thorbjørn Jelgren <thorbjorn@therightfoot.dk>
2025-07-26 12:21:35 +0200

    Atom: link-button

---
+++ app/web/components/atoms/link-button/link-button.js
@@ -0,0 +1,28 @@
+import { BaseComponent }  from '/js/BaseComponent.js'
+
+class AppLinkButton extends BaseComponent {
+  static get observedAttributes() {
+    return ['href', 'target', 'rel', 'aria-label'];
+  }
+
+  async render() {
+    const href = this.getAttribute('href') || '#';
+    const target = this.getAttribute('target') || '_self';
+    const rel = this.getAttribute('rel') || (target === '_blank' ? 'noopener noreferrer' : '');
+    const ariaLabel = this.getAttribute('aria-label') || '';
+
+    const query = new URLSearchParams({ href, target, rel, ariaLabel }).toString();
+
+    const [html, css] = await Promise.all([
+      fetch(`/components/atoms/link-button?${query}`).then(res => res.text()),
+      fetch('/components/atoms/link-button/styles').then(res => res.text())
+    ]);
+
+    this.shadowRoot.innerHTML = `
+      <style>${css}</style>
+      ${html}
+    `;
+  }
+}
+
+customElements.define('app-link-button', AppLinkButton);
--- app/web/public/js/components.js
+++ app/web/public/js/components.js
@@ -1,5 +1,6 @@
 import '/components/atoms/image/script';
 import '/components/atoms/link/script';
+import '/components/atoms/link-button/script';
 
 import '/components/molecules/card/script';
 import '/components/molecules/logo/script';
