e1a10f8805f892f6c73b44776288e7136b78c3ac
Thorbjørn Jelgren <thorbjorn@therightfoot.dk>
2025-07-26 13:28:21 +0200

    Move logo to atoms

--- app/web/public/js/components.js
+++ app/web/public/js/components.js
@@ -2,6 +2,6 @@ import '/components/atoms/button/button-script';
 import '/components/atoms/image/image-script';
 import '/components/atoms/link/link-script';
 import '/components/atoms/link-button/link-button-script';
+import '/components/atoms/logo/logo-script';
 
 import '/components/molecules/card/card-script';
-import '/components/molecules/logo/logo-script';
